e0a941ceba1371ca1325c0fd2185f451e08edca6
malike <st.malike@gmail.com>
2023-01-21 09:29:55 +0100

    mock operator with integration tests

--- controllers/samplekind_controller.go
+++ controllers/samplekind_controller.go
@@ -41,15 +41,11 @@ type SampleKindReconciler struct {
 //+kubebuilder:rbac:groups=app.malike.kendeh.com,resources=samplekinds/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=app.malike.kendeh.com,resources=samplekinds/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SampleKind object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+//+kubebuilder:rbac:groups="apps",resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;delete;patch
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+
+
 func (r *SampleKindReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
